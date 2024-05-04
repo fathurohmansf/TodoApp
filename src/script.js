0080d9a47f5a6c9f8f0d434d77a2240e2a98487a
fathurohmansf <fathurrhm909@gmail.com>
2024-05-04 22:51:05 +0700

    Update rendering toggle Todo yang sudah dikerjakan menjadi coret

--- src/script.js
+++ src/script.js
@@ -7,24 +7,29 @@
   }
 ]
 */
-let todos = [{
-        id: 1,
-        title: 'Belajar DOM manipulation',
-        isCompleted: true,
-    },
-    {
-        id: 2,
-        title: 'Belajar event listener',
-        isCompleted: false,
-    },
-    {
-        id: 3,
-        title: 'Belajar web storage',
-        isCompleted: false,
-    },
-];
 
+// // array di bawah ini akan di hapus karna akan membuat web storage baru
+// let todos = [{
+//         id: 1,
+//         title: 'Belajar DOM manipulation',
+//         isCompleted: true,
+//     },
+//     {
+//         id: 2,
+//         title: 'Belajar event listener',
+//         isCompleted: false,
+//     },
+//     {
+//         id: 3,
+//         title: 'Belajar web storage',
+//         isCompleted: false,
+//     },
+// ];
 
+// Ini untuk menyimpan data web storage nya
+const TODO_KEY = 'todos';
+
+// Ini Func Todo app nya
 (function() {
     const todoList = document.getElementById('todoList');
     const createTodoForm = document.getElementById('createTodoForm');
@@ -32,6 +37,7 @@ let todos = [{
 
     console.log(createTodoForm)
 
+    //Ini untuk Event Submit
     createTodoForm.addEventListener('submit', (event) => {
         event.preventDefault();
 
@@ -44,9 +50,17 @@ let todos = [{
             isCompleted: false,
         };
 
+        // Dapatkan daftar TODO saat ini
+        const todos = getAllTodos();
+
         // Menyimpan TODO baru
         const newTodos = [...todos, newTodo];
-        todos = newTodos;
+        const newTodosString = JSON.stringify(newTodos);
+        localStorage.setItem(TODO_KEY, newTodosString);
+
+        // // Menyimpan TODO baru sudah di gantikan dengan yg di atas
+        // const newTodos = [...todos, newTodo];
+        // todos = newTodos;
 
         // Perbarui UI
         render();
@@ -55,6 +69,21 @@ let todos = [{
         todoTitleInput.value = '';
 
     });
+
+    function getAllTodos() {
+        // Dapatkan data yang tersimpan (string)
+        const todosString = localStorage.getItem(TODO_KEY);
+        console.log('getAllTodos', todosString);
+        // Konversi string ke Array
+        const todos = JSON.parse(todosString);
+
+        // Kembalikan Array kosong juka tidak ada yang tersimpan
+        if (Array.isArray(todos)) {
+            return todos;
+        } else {
+            return [];
+        }
+    }
 
 
 
@@ -77,6 +106,9 @@ let todos = [{
     }
 
     function render() {
+        const todos = getAllTodos();
+        console.log('render:', todos);
+
         // Kosongkan daftar TODO sebelum render TODO ke halaman web
         todoList.innerHTML = '';
 
@@ -98,10 +130,14 @@ let todos = [{
     }
 
     function afterRender() {
+        // fungsi bahwa Todo di coret karna telah di kerjakan
         const toggleCheckboxes = document.querySelectorAll('.toggle');
         for (const toggleCheckbox of toggleCheckboxes) {
             toggleCheckbox.addEventListener('change', () => {
                 const todoId = toggleCheckbox.dataset.id;
+
+                // Dapatkan daftar TODO saat ini
+                const todos = getAllTodos();
 
                 // Update todo agar ditandai selesai
                 if (toggleCheckbox.checked == true) {
@@ -113,7 +149,12 @@ let todos = [{
                         return todo;
                     });
 
-                    todos = updatedTodos;
+                    // Memperbarui daftar TODO
+                    const updatedTodosString = JSON.stringify(updatedTodos);
+                    localStorage.setItem(TODO_KEY, updatedTodosString);
+
+                    // di bawah ini ada kesalah paham an(masih di cari)
+                    //todos = updatedTodos;
                 }
                 // Update todo agar ditandai belum selesai
                 else {
@@ -125,7 +166,12 @@ let todos = [{
                         return todo;
                     });
 
-                    todos = updatedTodos;
+                    // Memperbarui daftar TODO
+                    const updatedTodosString = JSON.stringify(updatedTodos);
+                    localStorage.setItem(TODO_KEY, updatedTodosString);
+
+                    // di bawah ini ada kesalah paham an(masih di cari)
+                    // todos = updatedTodos;
                 }
 
 
@@ -133,6 +179,9 @@ let todos = [{
                 for (const destroyButton of destroyButtons) {
                     destroyButton.addEventListener('click', () => {
                         const todoId = destroyButton.dataset.id;
+
+                        // Dapatkan daftar TODO saat ini
+                        const todos = getAllTodos();
 
                         // Dapatkan TODO berdasarkan ID untuk memeriksa keberadaannya
                         const todo = todos.find((todo) => todo.id == todoId);
@@ -149,6 +198,7 @@ let todos = [{
                         });
                         todos = newTodos;
 
+
                         // Perbarui UI
                         render();
                     });
@@ -160,10 +210,14 @@ let todos = [{
             });
         }
 
+        // Fungsi untuk mendelete TODO per item
         const destroyButtons = document.querySelectorAll('.destroy');
         for (const destroyButton of destroyButtons) {
             destroyButton.addEventListener('click', () => {
                 const todoId = destroyButton.dataset.id;
+
+                // Dapatkan daftar TODO saat ini
+                const todos = getAllTodos();
 
                 //Dapatkan TODO berdasarkan ID untuk memeriksa keberadaan nya
                 const todo = todos.find((todo) => todo.id == todoId);
@@ -175,10 +229,12 @@ let todos = [{
                 }
 
                 //Penghapusan TODO
-                const newTodos = todos.filter((todo) => {
+                const updatedTodos = todos.filter((todo) => {
                     return todo.id != todoId;
                 });
-                todos = newTodos;
+                const updatedTodosString = JSON.stringify(updatedTodos);
+                localStorage.setItem(TODO_KEY, updatedTodosString);
+
 
                 //Perbarui UI
                 render();
